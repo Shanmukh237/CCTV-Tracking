db8582e95b6319bbfe784252ffc31d7f11d30bc6
Shanmukh237 <166124745+Shanmukh237@users.noreply.github.com>
2025-02-15 23:37:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,74 +1,21 @@
-import tensorflow as tf
-from tensorflow import keras
-import numpy as np
 import cv2
-from keras import models
-import numpy as np
+from simple_facerec import SimpleFacerec
 
+sfr = SimpleFacerec()
+sfr.load_encoding_images("images/praneeth")
 
-facedetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-cap=cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-font=cv2.FONT_HERSHEY_COMPLEX
-
-
-model = models.load_model('keras_model.h5')
-
-
-def get_className(classNo):
-	if classNo==0:
-		return "Chando"
-	elif classNo==1:
-		return "Tony Stark"
-
+cap = cv2.VideoCapture(2)
 while True:
-	sucess, imgOrignal=cap.read()
-	faces = facedetect.detectMultiScale(imgOrignal,1.3,5)
-	for x,y,w,h in faces:
-		crop_img=imgOrignal[y:y+h,x:x+h]
-		img=cv2.resize(crop_img, (224,224))
-		img=img.reshape(1, 224, 224, 3)
-		prediction=model.predict(img)
-		classIndex=model.predict_classes(img)
-		probabilityValue=np.amax(prediction)
-		if classIndex==0:
-			cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(0,255,0),2)
-			cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (0,255,0),-2)
-			cv2.putText(imgOrignal, str(get_className(classIndex)),(x,y-10), font, 0.75, (255,255,255),1, cv2.LINE_AA)
-		elif classIndex==1:
-			cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(0,255,0),2)
-			cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (0,255,0),-2)
-			cv2.putText(imgOrignal, str(get_className(classIndex)),(x,y-10), font, 0.75, (255,255,255),1, cv2.LINE_AA)
-
-		cv2.putText(imgOrignal,str(round(probabilityValue*100, 2))+"%" ,(180, 75), font, 0.75, (255,0,0),2, cv2.LINE_AA)
-	cv2.imshow("Result",imgOrignal)
-	k=cv2.waitKey(1)
-	if k==ord('q'):
-		break
-
+    ret, frame = cap.read()
+    face_locations, face_names = sfr.detect_known_faces(frame)
+    for face_loc, name in zip(face_locations, face_names):
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+        cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+    	break
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
